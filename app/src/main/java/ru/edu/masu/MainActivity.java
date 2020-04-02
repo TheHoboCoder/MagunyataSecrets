cdca81d54ed50586758a9ff23cc87a8a586c984c
TheHoboCoder <animator.x@yandex.ru>
2020-04-02 17:20:38 +0300

    Activity for QR code scanning added

--- app/src/main/java/ru/edu/masu/MainActivity.java
+++ app/src/main/java/ru/edu/masu/MainActivity.java
@@ -1,7 +1,13 @@
 package ru.edu.masu;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -10,6 +16,7 @@ import java.util.ArrayList;
 
 import ru.edu.masu.adapters.QuestItemRVMainAdapter;
 import ru.edu.masu.data.QuestItem;
+import ru.edu.masu.utils.ScanActivity;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -39,7 +46,40 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onBackPressed() {
-        super.onBackPressed();
-        //TODO: вывод диалога с подтверждением выхода
+        AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+               .setMessage(R.string.quitGame)
+               .setPositiveButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                   @Override
+                   public void onClick(DialogInterface dialog, int which) {
+                       dialog.dismiss();
+
+                   }
+                })
+                .setNegativeButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        MainActivity.super.onBackPressed();
+                    }
+                })
+                .create();
+        dialog.show();
+    }
+
+    //example using ScanActivity
+    //TODO:remove this
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == ScanActivity.REQUEST_SCAN_ACTIVITY &&
+                resultCode == ScanActivity.RESULT_BARCODE_FOUND ){
+            String resData = data.getStringExtra(ScanActivity.BARCODE_TEXT);
+            Toast toast = Toast.makeText(MainActivity.this, resData, Toast.LENGTH_LONG);
+            toast.show();
+        }
+    }
+
+    public void testCamera(View view) {
+        Intent intent = new Intent(MainActivity.this, ScanActivity.class);
+        startActivityForResult(intent,ScanActivity.REQUEST_SCAN_ACTIVITY);
     }
 }
---
+++ app/src/main/java/ru/edu/masu/utils/ScanActivity.java
@@ -0,0 +1,197 @@
+package ru.edu.masu.utils;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.ImageProxy;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import ru.edu.masu.R;
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.media.Image;
+import android.os.Bundle;
+import android.util.Size;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+
+
+public class ScanActivity extends AppCompatActivity {
+
+    public final static int PERMISSION_REQUEST_CODE = 42;
+    //request code
+    public final static int REQUEST_SCAN_ACTIVITY = 34;
+    //result code
+    public final static int RESULT_BARCODE_FOUND = 12;
+    public final static String BARCODE_TEXT = "BARCODE_TEXT";
+
+    private ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    private PreviewView cameraView;
+   private Preview imagePreview;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+        cameraView = findViewById(R.id.cameraView);
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        if(isPermissionsGranted()){
+            cameraView.post(new Runnable() {
+                @Override
+                public void run() {
+                    startCamera();
+                }
+            });
+        }
+        else{
+            ActivityCompat.requestPermissions(ScanActivity.this,
+                    new String[]{Manifest.permission.CAMERA},PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    private void startCamera(){
+
+      cameraProviderFuture.addListener(() -> {
+          try {
+              ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+              imagePreview = new Preview.Builder()
+                      //.setTargetResolution(new Size(480,640))
+                      .build();
+              imagePreview.setSurfaceProvider(cameraView.getPreviewSurfaceProvider());
+
+              CameraSelector cameraSelector = new CameraSelector.Builder()
+                      .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                      .build();
+
+              ImageAnalysis analysis = new ImageAnalysis.Builder()
+                      .setTargetResolution(new Size(1280,720))
+                      .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                      .build();
+              analysis.setAnalyzer(Executors.newSingleThreadExecutor(), new BarcodeAnalyzer());
+
+               cameraProvider.bindToLifecycle(ScanActivity.this,cameraSelector,imagePreview,analysis);
+
+          } catch (ExecutionException|InterruptedException e) {
+              e.printStackTrace();
+          }
+      },ContextCompat.getMainExecutor(this));
+
+    }
+
+    private class BarcodeAnalyzer implements ImageAnalysis.Analyzer{
+
+        FirebaseVisionBarcodeDetector detector;
+
+        public BarcodeAnalyzer(){
+            FirebaseVisionBarcodeDetectorOptions options = new FirebaseVisionBarcodeDetectorOptions.Builder()
+                                                .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
+                                                .build();
+            detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+        }
+
+        private int degreesToFirebaseRotation(int degrees) {
+            switch (degrees) {
+                case 0:
+                    return FirebaseVisionImageMetadata.ROTATION_0;
+                case 90:
+                    return FirebaseVisionImageMetadata.ROTATION_90;
+                case 180:
+                    return FirebaseVisionImageMetadata.ROTATION_180;
+                case 270:
+                    return FirebaseVisionImageMetadata.ROTATION_270;
+                default:
+                    throw new IllegalArgumentException(
+                            "Rotation must be 0, 90, 180, or 270.");
+            }
+        }
+
+        @Override
+        public void analyze(@NonNull ImageProxy image) {
+            @SuppressLint("UnsafeExperimentalUsageError")
+            Image img = image.getImage();
+            int firebaseDegreess = degreesToFirebaseRotation(image.getImageInfo().getRotationDegrees());
+            FirebaseVisionImage cur = FirebaseVisionImage.fromMediaImage(img,
+                    firebaseDegreess);
+            detector.detectInImage(cur)
+                    .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                            getData(firebaseVisionBarcodes);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast toast = Toast.makeText(ScanActivity.this, R.string.failure,Toast.LENGTH_LONG);
+                            toast.show();
+                            e.printStackTrace();
+                            finish();
+                        }
+                    });
+            image.close();
+
+        }
+
+        private void getData(List<FirebaseVisionBarcode> firebaseVisionBarcodes){
+            if(firebaseVisionBarcodes.size()>0){
+                FirebaseVisionBarcode barcode = firebaseVisionBarcodes.get(0);
+                String rawValue = barcode.getRawValue();
+                Intent resIntent = new Intent();
+                resIntent.putExtra(BARCODE_TEXT, rawValue);
+                setResult(RESULT_BARCODE_FOUND, resIntent);
+                finish();
+            }
+        }
+
+    }
+
+
+    private boolean isPermissionsGranted(){
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_GRANTED;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == PERMISSION_REQUEST_CODE){
+            if(isPermissionsGranted()){
+                cameraView.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        startCamera();
+                    }
+                });
+            }
+            else{
+                Toast toast = Toast.makeText(this,"Вы должны разрешить доступ к камере",Toast.LENGTH_SHORT);
+                toast.show();
+                finish();
+            }
+        }
+    }
+
+
+
+
+}
