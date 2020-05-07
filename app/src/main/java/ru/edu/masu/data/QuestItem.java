38bac60bf9de617a717bfc6098567b3e91eb5f6a
ZifRD <ziftech.ru@gmail.com>
2020-05-07 13:32:52 +0300

    ZifRD & Valeria Limar: QuestItems background changed
    Signed-off-by: ZifRD <ziftech.ru@gmail.com>

--- app/src/main/java/ru/edu/masu/data/QuestItem.java
+++ app/src/main/java/ru/edu/masu/data/QuestItem.java
@@ -4,15 +4,19 @@ public class QuestItem {
     private int image;
     private String name;
     private String status;
+    private int background;
 
-    public QuestItem(int image, String name, String status) {
+    public QuestItem(int image, String name, String status,int background) {
         this.image = image;
         this.name = name;
         this.status = status;
+        this.background = background;
     }
 
     public int getImage() { return image; }
     public String getName() { return name; }
     public String getStatus() { return status; }
+
+    public int getBackground() {return background; }
 }
 
