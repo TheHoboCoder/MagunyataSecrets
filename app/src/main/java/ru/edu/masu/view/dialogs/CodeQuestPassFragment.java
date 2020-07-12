b089a78f2ff16043ee048b86ab937028afda62d6
TheHoboCoder <animator.x@yandex.ru>
2020-07-12 18:27:14 +0300

    remade quest pass method

--- app/src/main/java/ru/edu/masu/view/dialogs/CodeQuestPassFragment.java
+++ app/src/main/java/ru/edu/masu/view/dialogs/CodeQuestPassFragment.java
@@ -2,28 +2,30 @@ package ru.edu.masu.view.dialogs;
 
 import android.os.Bundle;
 
+import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import ru.edu.masu.R;
 import ru.edu.masu.model.CodeQuestPass;
-import ru.edu.masu.model.IQuestFinished;
-import ru.edu.masu.viewmodel.BasicVMFactory;
-import ru.edu.masu.viewmodel.StringQuestPassVM;
+import ru.edu.masu.model.data.repository.QuestRepository;
+import ru.edu.masu.viewmodel.MainVM;
+import ru.edu.masu.viewmodel.MainVMFactory;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
-public class StringQuestPassFragment extends BasicDialogFragment {
+public class CodeQuestPassFragment extends BasicDialogFragment {
 
     private CodeQuestPass questPass;
-    private StringQuestPassVM questPassVM;
+    private MainVM questPassVM;
     private EditText editText;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        questPassVM = new ViewModelProvider(this, new BasicVMFactory(questPass))
-                .get(StringQuestPassVM.class);
+        questPassVM = new ViewModelProvider(requireActivity()).get(MainVM.class);
     }
 
     public void setQuestPass(CodeQuestPass questPass){
@@ -31,8 +33,8 @@ public class StringQuestPassFragment extends BasicDialogFragment {
     }
 
 
-    public static StringQuestPassFragment newInstance(CodeQuestPass questPass) {
-        StringQuestPassFragment fragment = new StringQuestPassFragment();
+    public static CodeQuestPassFragment newInstance(CodeQuestPass questPass) {
+        CodeQuestPassFragment fragment = new CodeQuestPassFragment();
         fragment.setQuestPass(questPass);
         Bundle args = packArgs(R.layout.pass_quest_fragment,R.string.pass_quest);
         fragment.setArguments(args);
@@ -46,7 +48,10 @@ public class StringQuestPassFragment extends BasicDialogFragment {
 
     @Override
     protected void onActionBtnClick(View v){
-        if(questPassVM.tryPass(editText.getText().toString())){
+        CodeQuestPass codeQuestPass = (CodeQuestPass) questPassVM.provideQuestPass();
+        codeQuestPass.enterCode(editText.getText().toString());
+        if(codeQuestPass.isPassed()){
+            questPassVM.check(codeQuestPass);
             this.dismiss();
         }
         else{
